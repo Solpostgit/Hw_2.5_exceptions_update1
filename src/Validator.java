077ac05f22020db3b0dcaf11ddcd33943d5ef1dd
Solpostgit <begemot_off@mail.ru>
2023-10-28 18:19:23 +0300

    Использовал регулярные выражения для проверки допустимых символов.

--- src/Validator.java
+++ src/Validator.java
@@ -1,9 +1,12 @@
 import exception.WrongLoginException;
 import exception.WrongPasswordException;
 
+import java.util.regex.Pattern;
+
 public class Validator {
-    private static final String LEGAL_CHARACTERS =
-            "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_0123456789";
+    //private static final String LEGAL_CHARACTERS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM_0123456789";
+
+    private static final Pattern LOGIN_PASSWORD_PATTERN = Pattern.compile("[a-zA-Z_0-9]+");
     static final int validLength = 20;
 
     public static void validateString(
@@ -16,22 +19,24 @@ public class Validator {
     }
 
     public static void validateLogin(String login) {
+        if (!LOGIN_PASSWORD_PATTERN.matcher(login).matches()) {
+            throw new WrongLoginException("login содержит недопустимые символы");
+        }
+        /*
         for (int i = 0; i < login.length(); i++) {
             if (LEGAL_CHARACTERS.indexOf(login.charAt(i)) == -1) {
                 throw new WrongLoginException("login содержит недопустимые символы");
             }
         }
-
+         */
         if (login.length() > validLength) {
             throw new WrongLoginException("Длина login больше допустимого значения " + validLength);
         }
     }
 
     public static void validatePassword(String password, String confirmPassword) {
-        for (int i = 0; i < password.length(); i++) {
-            if (LEGAL_CHARACTERS.indexOf(password.charAt(i)) == -1) {
-                throw new WrongPasswordException("password содержит недопустимые символы");
-            }
+        if (!LOGIN_PASSWORD_PATTERN.matcher(password).matches()) {
+            throw new WrongLoginException("password содержит недопустимые символы");
         }
 
         if (password.length() > validLength) {
